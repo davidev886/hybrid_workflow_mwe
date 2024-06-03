3903f0ac68f7e6650241085809585b6d72267a01
Davide Vodola <davide.vodola@basf.com>
2024-06-03 15:27:56 +0200

    Add afqmc + gpu

--- main_mwe.py
+++ main_mwe.py
@@ -34,6 +34,7 @@ if __name__ == "__main__":
     optimizer_type = "cudaq"
 
     do_vqe = False
+    do_afqmc = True
     num_active_orbitals = 6
     num_active_electrons = 8
 
@@ -59,6 +60,7 @@ if __name__ == "__main__":
         verbose=4
     )
     nocca, noccb = mol.nelec
+    mol_nelec = mol.nelec
     mf = scf.ROHF(mol)
     print("# saving chkfile to", os.path.join(ipie_input_dir, chkptfile_rohf))
     mf.chkfile = file_chk
@@ -124,21 +126,82 @@ if __name__ == "__main__":
     else:
         final_state_vector = np.loadtxt("o2_wf.dat", dtype=np.complex_)
 
-    from src.input_ipie import get_coeff_wf
+    if do_afqmc:
+        import cupy
+        from ipie.config import config
+        config.update_option("use_gpu", True)
 
-    coeff, occas, occbs = get_coeff_wf(final_state_vector,
-                                       (nocca_act, noccb_act))
+        from ipie.hamiltonians.generic import Generic as HamGeneric
+        from ipie.qmc.afqmc import AFQMC
+        from ipie.systems.generic import Generic
+        from ipie.trial_wavefunction.particle_hole import ParticleHoleNonChunked
+        from ipie.utils.from_pyscf import gen_ipie_input_from_pyscf_chk
 
-    # Need to write wavefunction to checkpoint file.
-    with h5py.File(file_chk, "r+") as fh5:
-        fh5["mcscf/ci_coeffs"] = coeff
-        fh5["mcscf/occs_alpha"] = occas
-        fh5["mcscf/occs_beta"] = occbs
+        from src.input_ipie import get_coeff_wf
 
-    # generate input file for ipie
-    from ipie.utils.from_pyscf import gen_ipie_input_from_pyscf_chk
-    gen_ipie_input_from_pyscf_chk(file_chk,
-                                  mcscf=True,
-                                  chol_cut=1e-1)
+        coeff, occas, occbs = get_coeff_wf(final_state_vector,
+                                           ncore_electrons=0,
+                                           n_elec=(nocca_act, noccb_act)
+                                           )
 
-    # look at minimal_afqmc
+        # Need to write wavefunction to checkpoint file.
+        with h5py.File(file_chk, "r+") as fh5:
+            fh5["mcscf/ci_coeffs"] = coeff
+            fh5["mcscf/occs_alpha"] = occas
+            fh5["mcscf/occs_beta"] = occbs
+
+        # generate input file for ipie
+        from ipie.utils.from_pyscf import gen_ipie_input_from_pyscf_chk
+        gen_ipie_input_from_pyscf_chk(file_chk,
+                                      mcscf=True,
+                                      chol_cut=1e-5)
+
+        with h5py.File("hamiltonian.h5") as fa:
+            chol = fa["LXmn"][()]
+            h1e = fa["hcore"][()]
+            e0 = fa["e0"][()]
+
+        num_basis = chol.shape[1]
+
+        system = Generic(nelec=mol_nelec)
+
+        num_chol = chol.shape[0]
+        ham = HamGeneric(
+            np.array([h1e, h1e]),
+            chol.transpose((1, 2, 0)).reshape((num_basis * num_basis, num_chol)),
+            e0,
+        )
+
+        # Build trial wavefunction
+        with h5py.File("wavefunction.h5", "r") as fh5:
+            coeff = fh5["ci_coeffs"][:]
+            occa = fh5["occ_alpha"][:]
+            occb = fh5["occ_beta"][:]
+
+        wavefunction = (coeff, occa, occb)
+        trial = ParticleHoleNonChunked(
+            wavefunction,
+            mol_nelec,
+            num_basis,
+            num_dets_for_props=len(wavefunction[0]),
+            verbose=True,
+        )
+        trial.compute_trial_energy = True
+        trial.build()
+        trial.half_rotate(ham)
+
+        afqmc_msd = AFQMC.build(
+            mol_nelec,
+            ham,
+            trial,
+            num_walkers=100,
+            num_steps_per_block=25,
+            num_blocks=10,
+            timestep=0.005,
+            stabilize_freq=5,
+            seed=96264512,
+            pop_control_freq=5,
+            verbose=True,
+        )
+        afqmc_msd.run()
+        afqmc_msd.finalise(verbose=True)
